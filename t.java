87e58a161dbce11b1fd2a1f276cfcd906d550cfc
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-25 17:41:31 +0530

    max depth with theory
    Co-Authored-By: not-just <114912156+not-just@users.noreply.github.com>

--- t.java
+++ t.java
@@ -1,32 +1,10 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        Stack<TreeNode> st= new Stack<>();
-        if(root==null) return ans;
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0; 
         
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode curr = st.pop();
-            ans.add(curr.val);
-            if(curr.right!=null) st.push(curr.right);
-            if(curr.left!=null) st.push(curr.left);
-        }
+        int lh = maxDepth(root.left); 
+        int rh = maxDepth(root.right); 
         
-        return ans;
+        return 1 + Math.max(lh, rh); 
     }
 }
